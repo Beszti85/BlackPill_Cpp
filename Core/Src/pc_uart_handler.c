76f7568160141bf9ae8f0ee6f9b38ac514cf0d4e
Beszti85 <csababesztercei@gmail.com>
2023-02-07 07:47:35 +0100

    update PC Uart AT CMD reading

--- Core/Src/pc_uart_handler.c
+++ Core/Src/pc_uart_handler.c
@@ -12,6 +12,8 @@
 extern PCA9685_Handler_t LedDriverHandle;
 extern UART_HandleTypeDef huart1;
 
+uint8_t PCUART_EspAtCmdNum = 0u;
+
 void PCUART_ProcessRxCmd( uint8_t* ptrBuffer )
 {
   if( !strncmp( ptrBuffer, "LED_TOGGLE", sizeof("LED_TOGGLE")-1 ) )
@@ -19,8 +21,23 @@ void PCUART_ProcessRxCmd( uint8_t* ptrBuffer )
     PCA9685_ToggleOutputEnable(&LedDriverHandle);
   }
   else
-  if( !strncmp( ptrBuffer, "ESP_RST", sizeof("ESP_RST")-1 ) )
+  if( !strncmp( ptrBuffer, "ESP_AT", sizeof("ESP_AT")-1 ) )
   {
-    ESP8266_ProcessAtCmd( &huart1, ESP8266_CMD_ID_RST );
+    // go to the end of the cmd prefix
+    ptrBuffer += sizeof("ESP_AT")-1;
+    // Check the end of the string
+    if( *ptrBuffer != '0' )
+    {
+      // Get the number
+      PCUART_EspAtCmdNum = *ptrBuffer - '0';
+      ptrBuffer++;
+      // Any further characters - number > 9
+      if( *ptrBuffer != '0' )
+      {
+        PCUART_EspAtCmdNum *= 10u;
+        PCUART_EspAtCmdNum += (*ptrBuffer - '0');
+      }
+      ESP8266_ProcessAtCmd( &huart1, ESP8266_CMD_ID_RST );
+    }
   }
 }
